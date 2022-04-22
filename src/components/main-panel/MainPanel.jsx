577db25c1add4f2fcd6c337b31ac2a6d9c40b089
Elxan Quliyev <qelxan177@gmail.com>
2022-04-22 14:05:26 -0700

    04.20.2022

--- src/components/main-panel/MainPanel.jsx
+++ src/components/main-panel/MainPanel.jsx
@@ -1,27 +1,57 @@
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
-import React from "react";
+import {
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  Textarea,
+  Input,
+} from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
 
-const MainPanel = () => {
+const MainPanel = ({ mainLang, changeLang }) => {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [lang, setLang] = useState("");
+  useEffect(() => {
+    changeLang({name,description,lang})
+  }, [changeLang,name,description,lang]);
+
+  // console.log("re render")
   return (
-    <Tabs>
-      <TabList>
-        <Tab>One</Tab>
-        <Tab>Two</Tab>
-        <Tab>Three</Tab>
-      </TabList>
-      <TabPanels>
-        <TabPanel>
-          <p>one!</p>
-        </TabPanel>
-        <TabPanel>
-          <p>two!</p>
-        </TabPanel>
-        <TabPanel>
-          <p>three!</p>
-        </TabPanel>
-      </TabPanels>
-    </Tabs>
+    <>
+      <Tabs>
+        <TabList>
+          {mainLang.map((l) => (
+            <Tab key={l.id}>{l.langKey}</Tab>
+          ))}
+        </TabList>
+        <TabPanels>
+          {mainLang.map((l) => (
+            <TabPanel key={l.id}>
+              <div className="">
+                <Input
+                  mb={3}
+                  type="text"
+                  onChange={(e) => {
+                    setName(e.target.value);
+                    setLang(l.langKey);
+                  }}
+                />
+                <Textarea
+                  onChange={(e) => {
+                    setDescription(e.target.value);
+                    setLang(l.langKey);
+                  }}
+                  type="text"
+                ></Textarea>
+              </div>
+            </TabPanel>
+          ))}
+        </TabPanels>
+      </Tabs>
+    </>
   );
 };
 
-export default MainPanel;
+export default React.memo(MainPanel);
